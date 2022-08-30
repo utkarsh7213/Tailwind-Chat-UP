a2bb5fede96547b54e7af27221d99a774367685e
Utkarsh Singh <utkarsh.singh@weenggs.com>
2022-08-30 23:31:47 +0530

    k

--- public/code.js
+++ public/code.js
@@ -1,150 +1,178 @@
-(function(){
+(function () {
+  const app = document.querySelector(".app");
+  const socket = io();
 
-	const app = document.querySelector(".app");
-	const socket = io();
+  var uname;
+  var username;
+  var room_id;
 
-	let uname;
+  app
+    .querySelector(".join-screen #join-user")
+    .addEventListener("click", function () {
+      const ele = document.getElementById("chat-screen");
+      const id_box = document.getElementById("id_box");
+      const current_username = document.getElementById("current_username");
+      ele.scrollTop = ele.scrollHeight;
+      username = app.querySelector(".join-screen #username").value;
+      room_id = app.querySelector("#room_id").value;
+      uname = username;
+      if (username.length == 0 || room_id.length == 0) {
+        console.log("return");
+        return;
+      }
+      socket.emit("newuser", { username, room_id });
+      id_box.innerText = room_id;
+      current_username.innerText = username;
+      socket.emit("online_users", room_id);
+      app.querySelector(".join-screen").classList.remove("active");
+      app.querySelector(".chat-screen").classList.add("active");
+    });
 
-	app.querySelector(".join-screen #join-user").addEventListener("click",function(){
-		let username = app.querySelector(".join-screen #username").value;
-		if(username.length == 0){
-			return;
-		}
-		socket.emit("newuser",username);
-		uname = username;
+  app
+    .querySelector(".chat-screen #send-message")
+    .addEventListener("click", function () {
+      let message = app.querySelector(".chat-screen #message-input").value;
+      if (message.length == 0) {
+        return;
+      }
+      renderMessage("my", {
+        username: uname,
+        text: message,
+      });
+      socket.emit("chat", {
+        username: uname,
+        text: message,
+        id: room_id,
+      });
+      app.querySelector(".chat-screen #message-input").value = "";
+      const ele = document.getElementById("chat-screen");
+      ele.scrollTop = ele.scrollHeight;
+    });
 
-		app.querySelector(".join-screen").classList.remove("active");
-		app.querySelector(".chat-screen").classList.add("active");
-	});
+  app
+    .querySelector(".chat-screen #exit-chat")
+    .addEventListener("click", function () {
+      socket.emit("exituser", { uname, room_id });
+      window.location.href = window.location.href;
+    });
 
-	app.querySelector(".chat-screen #send-message").addEventListener("click",function(){
-		let message = app.querySelector(".chat-screen #message-input").value;
-		if(message.length  == 0){
-			return;
-		}
-		renderMessage("my",{
-			username:uname,
-			text:message
-		});
-		socket.emit("chat",{
-			username:uname,
-			text:message
-		});
-		app.querySelector(".chat-screen #message-input").value = "";
-		const ele=document.getElementById('chat-screen')
-	    ele.scrollTo(0,document.body.scrollHeight)
+  socket.on("update", function (update) {
+    renderMessage("update", update);
+  });
 
-	});
+  socket.on("chat", function (message) {
+    renderMessage("other", message);
+    app.querySelector(".chat-screen #message-input").value = "";
+    const ele = document.getElementById("chat-screen");
+    ele.scrollTop = ele.scrollHeight;
+  });
 
-	app.querySelector(".chat-screen #exit-chat").addEventListener("click",function(){
-		socket.emit("exituser",uname);
-		window.location.href = window.location.href;
-	});
-
-	socket.on("update",function(update){
-		renderMessage("update",update);
-	});
-	
-	socket.on("chat",function(message){
-		renderMessage("other",message);
-	});
-
-	function renderMessage(type,message){
-		let messageContainer = app.querySelector(".chat-screen .messages");
-		if(type == "my"){
-			let el = document.createElement("div");
-			el.setAttribute("class","message my-message");
-			el.innerHTML = `
+  function renderMessage(type, message) {
+    let messageContainer = app.querySelector(".chat-screen .messages");
+    if (type == "my") {
+      let el = document.createElement("div");
+      el.setAttribute("class", "message my-message");
+      el.innerHTML = `
 			<div class="flex justify-end my-2 animatecss-slideInRight  duration-200">
-			<div class=" border-[#26408b] border-2 p-4 rounded-2xl rounded-br-none  max-w-md   ">
-					<div class="name  text-teal-500">You</div>
-					<span class="text-[#dddedf] font-Nunito break-words">${message.text}</span>
-				</div>
-		</div>
+        <div class=" bg-[#23404e]  p-4 rounded-2xl rounded-br-none  max-w-md   ">
+            <div class="name  text-teal-500">You</div>
+            <span class="text-[#dddedf] font-Nunito break-words">${message.text}</span>
+          </div>
+      </div>
 			`;
-			messageContainer.appendChild(el);
-		} else if(type == "other"){
-			let el = document.createElement("div");
-			el.setAttribute("class","message other-message");
-			el.innerHTML = `
+      messageContainer.appendChild(el);
+    } else if (type == "other") {
+      let el = document.createElement("div");
+      el.setAttribute("class", "message other-message");
+      el.innerHTML = `
 			<div class="flex justify-start my-2 animatecss-slideInLeft  duration-200">
-                    <div class=" border-[#26408b] border-2 p-4 rounded-2xl rounded-bl-sm  max-w-md   ">
-                            <div class="name  text-[#b388eb]">${message.username}</div>
-                            <span class="text-[#dcdcdc] font-Nunito break-words">${message.text}</span>
-                        </div>
-                </div>
+          <div class=" bg-[#019393] p-4 rounded-2xl rounded-bl-sm  max-w-md   ">
+                  <div class="name  text-gray-900">${message.username}</div>
+                  <span class="text-[#dcdcdc] font-Nunito break-words">${message.text}</span>
+              </div>
+      </div>
 			`;
-			messageContainer.appendChild(el);
-		} else if(type == "update"){
-			let el = document.createElement("div");
-			el.setAttribute("class","update");
-			el.innerHTML = `
+      messageContainer.appendChild(el);
+    } else if (type == "update") {
+      let el = document.createElement("div");
+      el.setAttribute("class", "update");
+      el.innerHTML = `
 				<div>
 					<div class="text-red-500 text-center">${message}</div>
 				</div>
 			`;
-			messageContainer.appendChild(el);
-		}
-		// scroll chat to end
-		messageContainer.scrollTop = messageContainer.scrollHeight - messageContainer.clientHeight;
-	}
-
+      messageContainer.appendChild(el);
+    }
+    // scroll chat to end
+    app.querySelector(".chat-screen #message-input").value = "";
+    const ele = document.getElementById("chat-screen");
+    ele.scrollTop = ele.scrollHeight;
+  }
+  socket.on("recieve_users", function (users) {
+    const div = document.getElementById("online_user_div");
+    div.innerHTML = "";
+    users.forEach((e) => {
+      div.innerHTML += `   <div class="flex space-x-3 rounded-2xl  border border-green-300 items-center p-1 px-2 " id="">
+          <p class="h-5 w-5 rounded-full bg-green-500 "></p>
+          <span class="font-Nunito text-slate-200 pt-[2px]">${e}</span>
+        </div>`;
+    });
+  });
 })();
 
-let bgHead = document.getElementById('chat-screen');
-let btn1 = document.getElementById('btn1');
-let btn2 = document.getElementById('btn2');
-let btn3 = document.getElementById('btn3');
-let btn4 = document.getElementById('btn4');
-let btn5 = document.getElementById('btn5');
-
-
-btn1.addEventListener('click', function() {
-    bgHead.style.backgroundColor = "#03045e";
+$("#click1").click(function () {
+  $(".u7").hide();
+});
+$("#click").click(function () {
+  $(".u7").show();
 });
 
-btn2.addEventListener('click', function() {
-    bgHead.style.backgroundColor = "#240046";
+$(".loader").fadeTo(1500, 1).fadeOut(200);
+
+$("#message-input").keypress(function (event) {
+  if (event.keyCode === 13) {
+    $("#send-message").click();
+  }
 });
 
-btn3.addEventListener('click', function() {
-    bgHead.style.backgroundColor = "black";
+let bgHead = document.getElementById("chat-screen");
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
+let btn5 = document.getElementById("btn5");
+
+btn1.addEventListener("click", function () {
+  bgHead.style.backgroundColor = "#03045e";
 });
 
-btn4.addEventListener('click', function() {
-    bgHead.style.backgroundColor = "#1c0061";
+btn2.addEventListener("click", function () {
+  bgHead.style.backgroundColor = "#240046";
 });
 
-btn5.addEventListener('click', function() {
-    bgHead.style.backgroundColor = "#2f2235";
+btn3.addEventListener("click", function () {
+  bgHead.style.backgroundColor = "black";
 });
-btn6.addEventListener('click', function() {
-    bgHead.style.backgroundColor = "#373d20";
+
+btn4.addEventListener("click", function () {
+  bgHead.style.backgroundColor = "#1c0061";
 });
-btn7.addEventListener('click', function() {
-    bgHead.style.backgroundColor = "#5F093D";
+
+btn5.addEventListener("click", function () {
+  bgHead.style.backgroundColor = "#2f2235";
 });
-btn8.addEventListener('click', function() {
-    bgHead.style.backgroundColor = "#5E376D";
+btn6.addEventListener("click", function () {
+  bgHead.style.backgroundColor = "#373d20";
 });
-btn9.addEventListener('click', function() {
-    bgHead.style.backgroundColor = "#0D659D";
+btn7.addEventListener("click", function () {
+  bgHead.style.backgroundColor = "#5F093D";
+});
+btn8.addEventListener("click", function () {
+  bgHead.style.backgroundColor = "#5E376D";
+});
+btn9.addEventListener("click", function () {
+  bgHead.style.backgroundColor = "#0D659D";
 });
 // bgHead.addEventListener('click', function() {
 //     bgHead.style.backgroundColor = "#FFF";
 // });
-
-$("#click1").click(function(){
-    $(".u7").hide();
-  });
-  $("#click").click(function(){
-      $(".u7").show();
-    });
-
-	$(".loader").fadeTo(1500,1).fadeOut(200);
-
-	$("#message-input").keypress(function(event) {
-		if (event.keyCode === 13) {
-			$("#send-message").click();
-		}
-	});
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const e = require("express");
 const express = require("express");
 const path = require("path");
 
@@ -6,18 +7,48 @@ const server = require("http").createServer(app);
 
 const io = require("socket.io")(server);
 
-app.use(express.static(path.join(__dirname+"/public")));
+app.use(express.static(path.join(__dirname + "/public")));
 
-io.on("connection", function(socket){
-	socket.on("newuser",function(username){
-		socket.broadcast.emit("update", username + " joined the conversation");
-	});
-	socket.on("exituser",function(username){
-		socket.broadcast.emit("update", username + " left the conversation");
-	});
-	socket.on("chat",function(message){
-		socket.broadcast.emit("chat", message);
-	});
+var users = [];
+
+io.on("connection", function (socket) {
+  socket.on("newuser", function (data) {
+    users.push({
+      id: data.room_id,
+      username: data.username,
+    });
+    socket.join(data.room_id);
+    socket
+      .to(data.room_id)
+      .emit("update", data.username + " joined the conversation");
+    update_users(data.room_id);
+  });
+  socket.on("exituser", function (data) {
+    const indexOfObject = users.findIndex((object) => {
+      return object.username === data.uname;
+    });
+    users.splice(indexOfObject, 1);
+    socket
+      .to(data.room_id)
+      .emit("update", data.uname + " left the conversation");
+    update_users(data.room_id);
+  });
+  socket.on("chat", function (message) {
+    socket.to(message.id).emit("chat", message);
+  });
+  socket.on("online_users", function (user_id) {
+    update_users(user_id);
+  });
+
+  function update_users(userid) {
+    let user_data = [];
+    users.forEach((element) => {
+      if (element.id == userid) {
+        user_data.push(element.username);
+      }
+    });
+    io.sockets.in(userid).emit("recieve_users", user_data);
+  }
 });
 
 server.listen(5000);
